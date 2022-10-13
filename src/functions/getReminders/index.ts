475b2fea6d5724c9ae54966c5e9bd28538095f31
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-10-13 06:01:38 -0400

    Implement dynamo query

---
+++ src/functions/getReminders/index.ts
@@ -0,0 +1,31 @@
+import { formatJSONResponse } from '@libs/apiGateway';
+import { dynamo } from '@libs/dynamo';
+import { APIGatewayProxyEvent } from 'aws-lambda';
+
+export const handler = async (event: APIGatewayProxyEvent) => {
+  try {
+    const tableName = process.env.reminderTable;
+    const {userId} = event.pathParameters || {};
+
+    if(!userId){
+      return formatJSONResponse({
+        statusCode: 400,
+        data: {
+          message: 'Missing userId in path parameter'
+        }
+      });
+    }
+
+    const data = await dynamo.query({ tableName, index: 'index1', pkValue: userId});
+
+    return formatJSONResponse({data});
+  } catch(error){
+    console.log('Error', error);
+    return formatJSONResponse({
+      statusCode: 502,
+      data: {
+        message: error.message
+      }
+    });
+  }
+};
