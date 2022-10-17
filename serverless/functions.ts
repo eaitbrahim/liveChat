49ab91b76cc7b8239fb7a46d0e881e8cd040b6b7
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-10-17 08:11:09 -0400

    Define send method of websocket

--- serverless/functions.ts
+++ serverless/functions.ts
@@ -1,50 +1,16 @@
 import type { AWS } from "@serverless/typescript";
 
 const functions: AWS["functions"] = {
-  setReminder: {
+  createRoom: {
     handler: "src/functions/setReminder/index.handler",
     events: [
       {
-        httpApi: {
-          path: "/",
-          method: "post"
+        websocket: {
+          route: 'createRoom'
         }
       }
     ]
   },
-  getReminders: {
-    handler: "src/functions/getReminders/index.handler",
-    events: [
-      {
-        httpApi: {
-          path: "/{userId}",
-          method: "get"
-        }
-      }
-    ]
-  },
-  sendReminder: {
-    handler: "src/functions/sendReminder/index.handler",
-    events: [
-      {
-        stream: {
-          type: "dynamodb",
-          arn: {
-            "Fn::GetAtt": ["roomConnectionTable", "StreamArn"]
-          },
-          filterPatterns: [ { eventName: ["REMOVE"] } ] 
-        }
-      }
-    ],
-    //@ts-expect-error
-    iamRoleStatements: [
-      {
-        Effect: 'Allow',
-        Action: ['ses:sendEmail', 'sns:Publish'],
-        Resource: '*'
-      }
-    ]
-  }
 };
 
 export default functions;
